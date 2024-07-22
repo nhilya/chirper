b2992c4b0045e721c7a56f725c346d3015ca8c65
nhilya <nuhailya.dev@gmail.com>
2024-07-22 12:42:11 +0800

    Rendering the blade view to chirps.index and create a Blade view template with a form for users to create a new Chirps.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,18 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Foundation\Exceptions\RegisterErrorViewPaths;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response; // import statement that allows you to access the Response class within your Laravel application without needing to use the fully qualified namespace each time I wanna reference it.
+use  Illuminate\View\View; // import statement that is used to manage and render views.
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index(): View
     {
-        // testing the route, the output can be seen when redirect to http://chirper.test/chirps.
-        return response('Hello, I wanna learn Laravel!');
+        // redirect the user to the chirps.index view
+        return view('chirps.index');
     }
 
     /**
