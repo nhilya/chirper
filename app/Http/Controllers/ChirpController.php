e3fd0506c605fe7bae344e792c094e08eee959fc
nhilya <nuhailya.dev@gmail.com>
2024-07-22 14:26:17 +0800

    Updating the store method on ChirpController class to validate and store the data and create a new Chirp.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse; // import class is responsible for generating HTTP redirect responses. Redirect responses are used to navigate users to a different URL.
 use Illuminate\Foundation\Exceptions\RegisterErrorViewPaths;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response; // import statement that allows you to access the Response class within your Laravel application without needing to use the fully qualified namespace each time I wanna reference it.
@@ -30,9 +31,16 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+     public function store(Request $request): RedirectResponse
     {
-        //
+        // validate the request and ensure the user provides messages with max 255 chars.
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+        // creating a record that will belong to the authenticated user
+        $request->user()->chirps()->create($validated);
+        //return the reditect response to send users back to the chirps.index route.
+        return redirect(route('chirps.index'));
     }
 
     /**
