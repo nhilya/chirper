5eb88cc8c7050197b3e44e47a35d2d952fefc603
nhilya <nuhailya.dev@gmail.com>
2024-07-23 15:53:58 +0800

    Updated the users table, created the chirps table that linked together using user_id as a foreign key.

--- database/migrations/2024_07_21_070206_create_chirps_table.php
+++ database/migrations/2024_07_21_070206_create_chirps_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // update the table to create the relationship between users table with the chirps table.
         Schema::create('chirps', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // created the auto-incremented id column.
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete(); // created a user_id column (as a foreign key) that linking user_id to the is column on the users table.
+            $table->string('message'); // adding the message column that will storing the chirp message.
+            $table->timestamps(); // timestamp columns created to the table, which will auto managed by Eloquent to track each record that is created and updated.
         });
     }
 
