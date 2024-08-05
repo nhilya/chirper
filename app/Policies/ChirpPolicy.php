747f51aed78130ad42b059cd0b1467386313f85f
nhilya <nuhailya.dev@gmail.com>
2024-08-05 18:28:05 +0800

    Adding policy class that only allows the user to edit their own Chirps only.

---
+++ app/Policies/ChirpPolicy.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Chirp;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ChirpPolicy
+{
+    
+    use HandlesAuthorization;
+    
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Chirp $chirp): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Chirp $chirp): bool
+    {
+        // check if the user is the author of the Chirp
+        return @$chirp->user()->is($user);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Chirp $chirp): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Chirp $chirp): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Chirp $chirp): bool
+    {
+        //
+    }
+}
